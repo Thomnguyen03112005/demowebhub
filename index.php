527a6efdd7d25b4c4e40123a8948649dd2438d29
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-18 22:14:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <title>Thowm Sever Website</title>
         <head>
-         <link rel="shortcut icon" href="image/iconn.jpg" type="image/x-icon">  
+         <link rel="shortcut icon" href="./iconn.jpg" type="image/x-icon">  
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         </head>
